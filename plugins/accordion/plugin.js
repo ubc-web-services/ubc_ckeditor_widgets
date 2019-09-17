e38eb487247ba585a1dfb6e6a0d28632ccafe0d2
O'Toole, James <james.otoole@ubc.ca>
2019-09-17 15:05:13 -0700

    Update Accordion plugin

--- plugins/accordion/plugin.js
+++ plugins/accordion/plugin.js
@@ -1,35 +1,35 @@
 (function($) {
-    CKEDITOR.plugins.add('accordion', {
+    CKEDITOR.plugins.add('widget-accordion', {
         requires: 'widget',
         init: function(editor) {
             // Register the toolbar buttons for the CKEditor editor instance.
-            editor.ui.addButton('accordion', {
+            editor.ui.addButton('widget-accordion', {
                 label: 'Insert Accordion',
                 icon: this.path + 'icons/accordion.png',
-                command: 'accordion'
+                command: 'widget-accordion'
             });
             // Add our plugin-specific CSS to style the widget within CKEditor.
             editor.addContentsCss(this.path + 'css/editor-accordion.css');
             // Register the widget.
-            editor.widgets.add('accordion', {
-                template: '<div class="widget-accordion accordion accordion--closed">' +
-                    '<div class="accordion__tab" tabindex="0">Accordion title goes in here</div>' +
-                    '<div class="accordion__content clearfix"><p>Accordion contents happen in here</p></div>' +
+            editor.widgets.add('widget-accordion', {
+                template: '<div class="widget-accordion">' +
+                    '<div class="accordion accordion__heading js-reveal__parent"><div class="accordion__trigger js-reveal__trigger" aria-expanded="false">Accordion title goes in here</div></div>' +
+                    '<div class="accordion__content js-reveal__target clearfix" hidden><p>Accordion contents happen in here</p></div>' +
                     '</div>',
                 editables: {
                     title: {
-                        selector: '.accordion__tab',
+                        selector: '.accordion__trigger',
                         allowedContent: 'em'
                     },
                     content: {
                         selector: '.accordion__content'
                     }
                 },
-                allowedContent: 'div(!accordion,!widget-accordion,accordion--closed); div(!accordion__content,clearfix); div(!accordion__tab)[tabindex]',
+                allowedContent: 'div(!widget-accordion); div(!accordion,!accordion__heading,!js-reveal__parent); div(!accordion__trigger,!js-reveal__trigger); div(!accordion__content,!js-reveal__target,clearfix)[hidden]',
                 //
                 //      requiredContent: 'div(accordion)',
                 upcast: function(element) {
-                    return element.name == 'div' && element.hasClass('accordion');
+                    return element.name == 'div' && element.hasClass('widget-accordion');
                 }
             });
         }
