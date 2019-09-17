cfae87ad7530694a51a54a901f9ee7d51a3b30ad
O'Toole, James <james.otoole@ubc.ca>
2019-09-17 15:25:04 -0700

    Update plugin.js

--- plugins/accordiontoggle/plugin.js
+++ plugins/accordiontoggle/plugin.js
@@ -1,24 +1,24 @@
 (function($) {
-    CKEDITOR.plugins.add('accordiontoggle', {
+    CKEDITOR.plugins.add('widget-accordiontoggle', {
         requires: 'widget',
         init: function(editor) {
             // Register the toolbar buttons for the CKEditor editor instance.
-            editor.ui.addButton('accordiontoggle', {
+            editor.ui.addButton('widget-accordiontoggle', {
                 label: 'Insert Accordion Open | Close All Button',
                 icon: this.path + 'icons/accordiontoggle.png',
-                command: 'accordiontoggle'
+                command: 'widget-accordiontoggle'
             });
             // Add our plugin-specific CSS to style the widget within CKEditor.
             editor.addContentsCss(this.path + 'css/editor-accordiontoggle.css');
             // Register the widget.
-            editor.widgets.add('accordiontoggle', {
-                template: '<button class="widget-expandcollapse" data-state="0">Open All Accordions</button>',
+            editor.widgets.add('widget-accordiontoggle', {
+                template: '<button class="widget-expandcollapse unit-button unit-button--mid text-xs" data-state="0">Open All Accordions</button>',
                 editables: {
                     content: {
                         selector: '.expandcollapse'
                     }
                 },
-                allowedContent: 'button(!widget-expandcollapse)[!data-state]',
+                allowedContent: 'button(!widget-expandcollapse,unit-button,unit-button--mid,text-xs)[!data-state]',
                 upcast: function(element) {
                     return element.name == 'button' && element.hasClass('widget-expandcollapse');
                 }
