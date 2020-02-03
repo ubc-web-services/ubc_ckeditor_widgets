3431a02bb1d165a5a60c069d6625c8f032a81dd8
nandini <nandini.bulusu@ubc.ca>
2020-02-03 15:50:24 -0800

    Trying to fix widget error

--- plugins/widget-quote/plugin.js
+++ plugins/widget-quote/plugin.js
@@ -17,9 +17,8 @@
         '<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</blockquote>' +
         '<cite>John Smith, Manager</cite>' +
         '</div>',
-        allowedContent: 'div(!quote);',
+        allowedContent: 'div(!quote);blockquote(*);cite(*);',
         // Minimum HTML which is required by this widget to work.
-        requiredContent: 'div(quote)',
         editables: {
           quote: {
             selector:'blockquote',
