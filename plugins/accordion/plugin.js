f19795db078cdfa014611488c189960cf79bb357
James O'Toole <james.otoole@ubc.ca>
2020-03-11 11:59:38 -0700

    Add heading back into js trigger, add editor heading styles

--- plugins/accordion/plugin.js
+++ plugins/accordion/plugin.js
@@ -13,19 +13,21 @@
             // Register the widget.
             editor.widgets.add('accordion', {
                 template: '<div class="widget-accordion">' +
-                    '<div class="accordion js-reveal__parent"><div class="js-reveal__trigger accordion__trigger" aria-expanded="false">Accordion title goes in here</div></div>' +
+                    '<div class="accordion js-reveal__parent"><div class="js-reveal__trigger accordion__trigger" aria-expanded="false"><h3>Accordion title goes in here</h3></div></div>' +
                     '<div class="js-reveal__target accordion__content clearfix" hidden><p>Accordion contents happen in here</p></div>' +
                     '</div>',
                 editables: {
                     title: {
                         selector: '.accordion__trigger',
-                        allowedContent: 'em;h3;h4;h5;h6'
+                        allowedContent: 'h3;h4;h5;h6;em'
                     },
                     content: {
                         selector: '.accordion__content'
                     }
                 },
-                allowedContent: 'div(!widget-accordion); div(!accordion,!js-reveal__parent);div(!js-reveal__trigger,!accordion__trigger)[aria-expanded];div(!js-reveal__target,!accordion__content)[hidden]',
+                allowedContent: 'div(!widget-accordion);div(!accordion,!js-reveal__parent);div(!js-reveal__trigger,!accordion__trigger)[aria-expanded];div(!js-reveal__target,!accordion__content)[hidden]',
+                //
+                //      requiredContent: 'div(accordion)',
                 upcast: function(element) {
                     return element.name == 'div' && element.hasClass('widget-accordion');
                 }
