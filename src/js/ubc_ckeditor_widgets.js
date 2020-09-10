1509a35c613726a77f1a973a717d5ca87f01c84d
James O'Toole <james.otoole@ubc.ca>
2020-09-10 16:27:50 -0700

    Convert object into array so we can iterate with forEach

--- src/js/ubc_ckeditor_widgets.js
+++ src/js/ubc_ckeditor_widgets.js
@@ -6,16 +6,16 @@
       }
       toggleAccordion() {
         const accordionItem = document.querySelectorAll(this.node);
-        accordionItem.forEach((item, key) => {
-          let btn = item.querySelector('.js-reveal__trigger');
+        Object.keys(accordionItem).forEach((key) => {
+          let btn = key.querySelector('.js-reveal__trigger');
           btn.addEventListener('click', () => {
-            let target = item.querySelector('.js-reveal__target');
+            let target = key.querySelector('.js-reveal__target');
             let expanded = btn.getAttribute('aria-expanded') === 'true';
             btn.setAttribute('aria-expanded', !expanded);
             target.hidden.toggle;
             target.hidden = expanded;
             btn.classList.toggle('is-open');
-            item.classList.toggle('is-open');
+            key.classList.toggle('is-open');
           });
         });
       }
