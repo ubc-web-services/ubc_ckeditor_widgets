beca944298c467665a7dc25e5be46278f40c1d58
James O'Toole <james.otoole@ubc.ca>
2021-02-03 13:57:37 -0800

    Fix for firing twice on click (use context instead of document)

--- src/js/ubc_ckeditor_widgets.js
+++ src/js/ubc_ckeditor_widgets.js
@@ -1,23 +1,15 @@
 (function (Drupal) {
   Drupal.behaviors.ubcCkeditorWidgets = {
     attach: function (context, settings) {
-      document.querySelectorAll('.widget-accordion').forEach(item => {
-        let btn = item.querySelector('.js-reveal__trigger');
+      context.querySelectorAll('.widget-accordion').forEach(item => {
+        var btn = item.querySelector('.js-reveal__trigger');
         btn.addEventListener('click', () => {
-          let expanded = btn.getAttribute('aria-expanded') === 'true';
-          let target = item.querySelector('.js-reveal__target');
+          var expanded = btn.getAttribute('aria-expanded') === 'true';
+          var target = item.querySelector('.js-reveal__target');
           btn.setAttribute('aria-expanded', !expanded);
           btn.classList.toggle('is-open');
           item.classList.toggle('is-open');
           target.toggleAttribute('hidden');
-          target.animate([
-            { transition: 'opacity', opacity: '0' },
-            { opacity: '100' }
-          ], {
-            duration: 250,
-            easing: 'linear',
-            iterations: 1
-          });
         });
       });
     }
