7daeab5ce1093b74a8419bc70204c5b9007df007
James O'Toole <james.otoole@ubc.ca>
2021-02-03 11:55:00 -0800

    Update accordion toggle script again(!) with behaviours, animation

--- src/js/ubc_ckeditor_widgets.js
+++ src/js/ubc_ckeditor_widgets.js
@@ -1,25 +1,25 @@
-(function () {
-    'use strict';
-    class Accordion {
-      constructor(node) {
-        this.node = node;
-      }
-      toggleAccordion() {
-        const accordionItem = document.querySelectorAll(this.node);
-        Object.keys(accordionItem).forEach((key) => {
-          let btn = key.querySelector('.js-reveal__trigger');
-          btn.addEventListener('click', () => {
-            let target = key.querySelector('.js-reveal__target');
-            let expanded = btn.getAttribute('aria-expanded') === 'true';
-            btn.setAttribute('aria-expanded', !expanded);
-            target.hidden.toggle;
-            target.hidden = expanded;
-            btn.classList.toggle('is-open');
-            key.classList.toggle('is-open');
+(function (Drupal) {
+  Drupal.behaviors.ubcCkeditorWidgets = {
+    attach: function (context, settings) {
+      document.querySelectorAll('.widget-accordion').forEach(item => {
+        let btn = item.querySelector('.js-reveal__trigger');
+        btn.addEventListener('click', () => {
+          let expanded = btn.getAttribute('aria-expanded') === 'true';
+          let target = item.querySelector('.js-reveal__target');
+          btn.setAttribute('aria-expanded', !expanded);
+          btn.classList.toggle('is-open');
+          item.classList.toggle('is-open');
+          target.toggleAttribute('hidden');
+          target.animate([
+            { transition: 'opacity', opacity: '0' },
+            { opacity: '100' }
+          ], {
+            duration: 250,
+            easing: 'linear',
+            iterations: 1
           });
         });
-      }
+      });
     }
-    const accordion = new Accordion('.widget-accordion');
-    accordion.toggleAccordion();
-  })();
+  };
+})(Drupal);
