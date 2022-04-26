4ce8c193f3046288d0e6c1bb16ad481f9ae60e65
James O'Toole <james.otoole@ubc.ca>
2022-04-26 13:25:58 -0700

    accordion updates
    update accordion to allow keyboard nav, update accordion toggle to correctly toggle regardless of state

--- src/js/ubc_ckeditor_widgets.js
+++ src/js/ubc_ckeditor_widgets.js
@@ -3,6 +3,7 @@
     attach: function (context, settings) {
       once('widgetAccordions', '.widget-accordion', context).forEach(item => {
         let btn = item.querySelector('.js-reveal__trigger');
+        btn.innerHTML = '<button>' + btn.innerHTML + '</button>';
         btn.addEventListener('click', () => {
           let expanded = btn.getAttribute('aria-expanded') === 'true';
           let target = item.querySelector('.js-reveal__target');
@@ -27,23 +28,33 @@
           const accordions = document.querySelectorAll('.widget-accordion');
           state = !state;
           item.innerText = text + ' All Accordions';
-          accordions.forEach(accordion => {
-            let btn = accordion.querySelector('.js-reveal__trigger');
-            let expanded = btn.getAttribute('aria-expanded');
-            let target = accordion.querySelector('.js-reveal__target');
-            btn.setAttribute('aria-expanded', !expanded);
-            btn.classList.toggle('is-open');
-            item.classList.toggle('is-open');
-            target.toggleAttribute('hidden');
-            target.animate([
-              { transition: 'opacity', opacity: '0' },
-              { opacity: '100' }
-            ], {
-              duration: 250,
-              easing: 'linear',
-              iterations: 1
+          if (state == "0") {
+            accordions.forEach(accordion => {
+              let btn = accordion.querySelector('.js-reveal__trigger');
+              let target = accordion.querySelector('.js-reveal__target');
+              btn.setAttribute('aria-expanded', 'true');
+              btn.classList.add('is-open');
+              item.classList.add('is-open');
+              target.removeAttribute('hidden');
+              target.animate([
+                { transition: 'opacity', opacity: '0' },
+                { opacity: '100' }
+              ], {
+                duration: 250,
+                easing: 'linear',
+                iterations: 1
+              });
             });
-          });
+          } else {
+            accordions.forEach(accordion => {
+              let btn = accordion.querySelector('.js-reveal__trigger');
+              let target = accordion.querySelector('.js-reveal__target');
+              btn.setAttribute('aria-expanded', 'false');
+              btn.classList.remove('is-open');
+              item.classList.remove('is-open');
+              target.setAttribute('hidden', '');
+            });
+          }
         });
       });
     }
